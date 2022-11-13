be8e07b9a2be28433af46618bd491bfa15a11e88
anthonydefine <101680125+anthonydefine@users.noreply.github.com>
2022-11-13 18:09:08 -0500

    Add files via upload

---
+++ gemini.js
@@ -0,0 +1,53 @@
+
+
+//Navbar active changing
+var navContainer = document.getElementById("navbarNav");
+
+// Get all buttons with class="btn" inside the container
+var btns = navContainer.getElementsByClassName("nav-link");
+
+// Loop through the buttons and add the active class to the current/clicked button
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+
+    // If there's no active class
+    if (current.length > 0) {
+      current[0].className = current[0].className.replace(" active", "");
+    }
+
+    // Add the active class to the current/clicked button
+    this.className += " active";
+  });
+}
+
+let mybutton = document.getElementById("scrollTopBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 220 ||
+    document.documentElement.scrollTop > 220
+  ) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+// When the user clicks on the button, scroll to the top of the document
+mybutton.addEventListener("click", backToTop);
+
+function backToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+
+$('#messageForm').on('submit', function(e){
+  $('#sprayModal').modal('show');
+  e.preventDefault();
+});
