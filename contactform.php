7d442c27a2aa10ba81def77a7aba4592209d5456
anthonydefine <101680125+anthonydefine@users.noreply.github.com>
2022-11-14 18:31:01 -0500

    Add files via upload

---
+++ contactform.php
@@ -0,0 +1,27 @@
+<?php
+
+$messageSent = false;
+
+if(isset($_POST['userEmail']) && $_POST['userEmail'] != ''){
+
+  if(filter_var($_POST['userEmail'], filter_validate_email)) {
+
+    $firstName = $_POST['first'];
+    $lastName = $_POST['last'];
+    $userEmail = $_POST['userEmail'];
+    $message = $_POST['message'];
+
+    $to = 'tboneisdabomb25@aol.com';
+    $messageSubject = "You have received a message from the website!";
+    $body = "";
+
+    $body .= "From: ".$firstName." ".$lastName. "\n\n";
+    $body .= "Email: ".$userEmail. "\n\n";
+    $body .= "Message: ".$message. "\n\n";
+
+    //mail($to,$messageSubject,$body);
+
+    $messageSent = true;
+  }
+
+}
